a58b2cbd858faf60e7d31dcf099f3e65705d7f3d
Christopher Chaison <cchaison0@gmail.com>
2020-03-17 23:42:01 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,11 @@
+def Appointment
+  attr_accessor :doctor, :patient
+
+  @@all = []
+  
+  def initialize(doctor, patient)
+    @doctor = doctor
+    @patient = patient
+    @@all << self
+  end
+end
